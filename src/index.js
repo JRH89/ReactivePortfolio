d8edefd0a7f1b82fd4b85038c725ab80d33ccaef
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-02-24 15:02:52 -0800

    skill section

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+import Skills from "./components/Skills";
+const App = () => {
+return (
+    <div className="App">
+      // Nav, Header, and other components should be here. I'm only importing my Skills component for styling. 
+      <Skills />
+    </div>
+  );
+};
+
+export default App;
